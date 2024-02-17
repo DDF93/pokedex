228cdc4819346b41c91ad689f9d798a5469635f0
Dominic Welds <dominicwelds@googlemail.com>
2024-02-17 15:31:55 +0000

    Seed made

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,12 @@ url = "https://api.pokemontcg.io/v2/cards/"
 
 cards = JSON.parse(URI.open(url).read)["data"]
 cards.each do |card|
+  card_price = card.dig("tcgplayer", "prices", "holofoil", "market") || rand(1..100)
   Card.create(
     name: card["name"],
     image_url: card["images"]["small"],
     rarity: card["rarity"],
-    price: card["tcgplayer"]["prices"]["holofoil"]["market"]
+    price: card_price
   )
 end
 "Completed"
