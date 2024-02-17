2acf0dcea87f30547885a790285fbb545329e5e8
Dominic Welds <dominicwelds@googlemail.com>
2024-02-17 22:11:50 +0000

    Added Card Show and linked to it from index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  skip_before_action :authenticate_user!, only: [:index, :show]
-
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,5 @@
 class CardsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @cards = Card.order("RANDOM()").limit(50)
   end
