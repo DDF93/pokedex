a2ab41707ded8c0722b599eda6b3e32131ee2c46
laura prattico <lauraprattico@hotmail.com>
2024-02-19 19:49:13 +0000

    create listings controller, implement new and create actions, add listings folder in views

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,23 @@
+class ListingsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = current_user.listings.new(listing_params)
+
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render :new
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:card_id)
+  end
+
+end
