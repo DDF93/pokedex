c78c4898d3281fd0af67cc38ba66804e85ef7712
Dominic Welds <dominicwelds@googlemail.com>
2024-02-22 23:38:47 +0000

    Changed card show slightly as the listing count wasn't excluding the sold listings and empty listing message wasn't displaying as it was picking up the sold listings

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -7,5 +7,6 @@ class CardsController < ApplicationController
   def show
     @card = Card.find(params[:id])
     @cards = Card.order("RANDOM()").limit(6)
+    @unsold_listings = @card.listings.reject(&:sold)
   end
 end
