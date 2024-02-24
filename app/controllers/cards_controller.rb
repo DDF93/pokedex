d6e53a61f38361a13d55f78038b9cba7d54e2884
Dominic Welds <dominicwelds@googlemail.com>
2024-02-24 09:18:06 +0000

    Started filter

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -2,6 +2,10 @@ class CardsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
   def index
     @cards = Card.order("RANDOM()").limit(50)
+    if params[:rarity].present? && params[:rarity] != "All"
+      @cards = Card.where(rarity: params[:rarity]).limit(50)
+    end
+    @rarities = Card.where.not(rarity: nil).pluck(:rarity).uniq.sort
   end
 
   def show
