985912b12eac80cd0f66b96c450cf1a23db27052
laura prattico <lauraprattico@hotmail.com>
2024-02-26 20:54:26 +0000

    built method for updating time in listing panel

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def time_ago_in_words_from_now(time)
+    distance_in_minutes = ((Time.now - time) / 60).round
+    case distance_in_minutes
+    when 0..1
+      "just now"
+    when 2..59
+      "#{distance_in_minutes} minutes ago"
+    when 60..90
+      "1 hour ago"
+    when 90..1440
+      "#{(distance_in_minutes / 60).round} hours ago"
+    else
+      "#{(distance_in_minutes / 1440).round} days ago"
+    end
+  end
 end
