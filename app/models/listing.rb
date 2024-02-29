69fd062bdd0eb215a77eb3e016bf5c33aaf71770
laura prattico <lauraprattico@hotmail.com>
2024-02-29 21:35:29 +0000

    updated address delivery

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,5 @@
 class Listing < ApplicationRecord
-  attr_accessor :post_code, :city, :state, :phone_number
+  attr_accessor :post_code, :city, :country, :phone_number
   belongs_to :user
   belongs_to :card
   has_one :sale
