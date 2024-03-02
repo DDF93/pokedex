9ff094618d9785a0772060c4fa83fa42067d16bf
Eleanor Vazraghi <eleanorvazraghi@gmail.com>
2024-03-02 11:52:46 +0000

    added colour system to rarity box

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,4 +14,21 @@ module ApplicationHelper
       "#{(distance_in_minutes / 1440).round} days ago"
     end
   end
+
+  def rarity_class(rarity)
+    case rarity
+    when "Common"
+      "rarity-common"
+    when "Uncommon"
+      "rarity-uncommon"
+    when "Rare", "Double Rare", "Rare ACE", "Rare BREAK", "Rare Holo", "Rare Holo EX", "Rare Holo GX", "Rare Holo LV.X", "Rare Holo Star", "Rare Holo V", "Rare Holo VMAX", "Rare Holo VSTAR", "Rare Prime", "Rare Prism Star", "Rare Ultra"
+      "rarity-rare"
+    when "Hyper Rare", "Rare Rainbow", "Rare Secret", "Rare Shining", "Rare Shiny", "Rare Shiny GX", "Shiny Rare", "Shiny Ultra Rare", "Ultra Rare"
+      "rarity-hyper-rare"
+    when "Promo", "Amazing Rare", "Classic Collection", "Illustration Rare", "LEGEND", "Radiant Rare", "Special Illustration Rare", "Trainer Gallery Rare Holo"
+      "rarity-special"
+    else
+      "rarity-common"
+    end
+  end
 end
